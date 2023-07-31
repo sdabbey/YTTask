c180b3215f60f3d7365749e2865ab6a01fdae576
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2023-07-31 17:15:14 +0000

    update with env and database

--- createsuperuser.py
+++ createsuperuser.py
@@ -7,7 +7,7 @@ import os
 from django.core.management import call_command
 from accounts.models import User
 def create_superuser():
-    password = os.environ.get('SUPERUSER_PASSWORD', 'default_password')
+    password = "testing321"
     call_command('createsuperuser', '--noinput', email='admin@yttask.com')
     user = User.objects.get(email='admin@yttask.com')
     user.set_password(password)
