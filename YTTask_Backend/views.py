afbb868e4d84b5ac57baa1b6e7a6e88c01783a3a
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2023-07-31 17:32:07 +0000

    update with env and database

--- YTTask_Backend/views.py
+++ YTTask_Backend/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from accounts.models import User
 def home(request):
     return render(request, "landingpage.html")
+
+
+def create_superuser(request):
+    password = "testing321"
+    User.objects.create_superuser(email='admin@yttask.com', password=password)
+    user = User.objects.get(email='admin@yttask.com')
+    user.set_password(password)
+    user.save()
+    return redirect("landingpage")
--- createsuperuser.py
+++
@@ -1,8 +0,0 @@
-
-from accounts.models import User
-def create_superuser():
-    password = "testing321"
-    User.objects.create_superuser(email='admin@yttask.com', password=password)
-    user = User.objects.get(email='admin@yttask.com')
-    user.set_password(password)
-    user.save()
