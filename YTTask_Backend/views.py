43b6284dba3f1d6295c9b0458d76fb20d14697e5
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2023-07-31 17:39:30 +0000

    update with env and database

--- YTTask_Backend/views.py
+++ YTTask_Backend/views.py
@@ -6,6 +6,8 @@ def home(request):
 
 def create_superuser(request):
     password = "testing321"
+    if User.objects.filter(email='admin@yttask.com').exists():
+        return redirect("landingpage")
     User.objects.create_superuser(email='admin@yttask.com', password=password)
     user = User.objects.get(email='admin@yttask.com')
     user.set_password(password)
