8b2d1a47336ec20da6e8794f2cdb457d08346577
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2023-08-07 00:48:48 +0000

    update

--- YTTask_Backend/settings.py
+++ YTTask_Backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django-insecure-*x-)$z#9_xoq$8l0d%8#-@tbi*lrfrhmxjfqj3vbi^t9&(z@=o"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
