504b52d0d66786c65b0a7cfaccd1992eb02f2d43
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2023-08-08 16:02:15 +0000

    update with payout system

--- dashboard/models.py
+++ dashboard/models.py
@@ -30,3 +30,18 @@ class YTTasker_task(models.Model):
 
 
 
+
+class Notification(models.Model):
+    info = models.CharField(max_length=500)
+    pub_date = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.info
+
+
+class FAQ(models.Model):
+    question = models.CharField(max_length=500)
+    answer = models.TextField()
+
+    def __str__(self):
+        return self.question
