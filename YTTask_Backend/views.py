edeab77ca100906da51e610b830ae6c65960dcdc
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2023-08-08 17:24:33 +0000

    update

--- YTTask_Backend/views.py
+++ YTTask_Backend/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from accounts.models import User
 from dashboard.models import Task
+import json
 def home(request):
     return render(request, "landingpage.html")
 
 def sitemap(request):
     return render(request, "sitemap.xml")
+
+def create_tasks(request):
+    # Read data from the JSON file
+    file_path = "templates/tasks.json"
+    with open(file_path) as json_file:
+        tasks_data = json.load(json_file)
+
+    # Loop through the tasks data and create Task objects
+    if request.user.is_superuser:
+        for prompt, secret_code in tasks_data.items():
+            Task.objects.create(
+                prompt=prompt,
+                secret_code=secret_code,
+                point=0.02  # Set the default value for the point field
+            )
+        return HttpResponse("Successfully created")
+
+    return HttpResponse("Access denied")
 
 def delete_tasks(request):
     tasks = Task.objects.all()
