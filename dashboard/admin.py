4db397d4f8554562b6aebd96be0969013821dd9c
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2023-08-09 17:23:36 +0000

    update

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Task)
-admin.site.register(YTTasker_task)
 admin.site.register(Notification)
 admin.site.register(FAQ)
