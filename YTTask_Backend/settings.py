df2186d01d30e4514bcf2683262ff6dac18797c2
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2023-08-25 20:20:43 +0000

    update

--- YTTask_Backend/settings.py
+++ YTTask_Backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-*x-)$z#9_xoq$8l0d%8#-@tbi*lrfrhmxjfqj3vbi^t9&(z@=o"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["yttask.up.railway.app", "www.yttasker.online"]
 CSRF_TRUSTED_ORIGINS = ['https://yttask.up.railway.app', 'https://www.yttasker.online']
